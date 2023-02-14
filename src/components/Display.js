7bf9cef0e6d48481f5ebeec17f4c07123d878bcb
Francis <francisbernal14@gmail.com>
2023-02-14 21:30:51 +0100

    feat: add styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,10 @@
 import { marked } from "marked"
+
+marked.use({
+    gfm: true,
+    breaks: true
+  })
+  
 export default function Display(props) {
     return (
         <div id="preview" dangerouslySetInnerHTML={{__html: marked(props.input)}}>
--- src/components/GetInput.js
+++ src/components/GetInput.js
@@ -1,12 +1,10 @@
 export default function GetInput(props) {
     return (
-        <div>
-            <textarea 
-                type="text"
-                id="editor" 
-                onChange={props.handleChange} 
-                value={props.value}>
-            </textarea>
-        </div>
+        <textarea
+            type="text"
+            id="editor"
+            onChange={props.handleChange}
+            value={props.value}>
+        </textarea>
     )
 }
